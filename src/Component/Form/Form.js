0190ff577c16751c2d6ab791eebfc6329114cd27
jonaRML <55372192+jonaRML@users.noreply.github.com>
2022-12-13 13:33:04 -0500

    Update Form.js

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -110,7 +110,7 @@ function Form() {
       !e.descripcion
     ) {
       
-      axios.post('http://localhost:3001/courses',input)
+      axios.post('https://cursort-production.up.railway.app/courses',input)
         .then(res => {
             onOpen();
             setInput({
